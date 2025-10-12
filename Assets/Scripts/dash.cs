631d06e079c03842eaf3823ef97312116da93327
Antoine Pezzulo <a.pezzulo@student.isartdigital.com>
2025-10-13 01:10:27 +0200

    Dash V-0.2 + Assets Fantasy

--- Assets/Scripts/dash.cs
+++ Assets/Scripts/dash.cs
@@ -7,28 +7,38 @@ namespace StarterAssets
     [RequireComponent(typeof(StarterAssetsInputs))]
     public class DashAbility : MonoBehaviour
     {
-        public float dashSpeed = 16f;
         public float dashDuration = 0.18f;
         public float dashCooldown = 0.6f;
+        public float dashSpeed = 16f;
+        public float dashDistance = 0f;
+        public string rollStateName = "Roll";
 
         CharacterController cc;
         StarterAssetsInputs inputs;
         Transform cam;
+        Animator anim;
         bool isDashing;
         float dashTime;
         float cd;
         Vector3 dashDir;
+        float speedThisDash;
+        int rollStateHash;
 
         void Awake()
         {
             cc = GetComponent<CharacterController>();
             inputs = GetComponent<StarterAssetsInputs>();
             cam = Camera.main ? Camera.main.transform : null;
+            anim = GetComponent<Animator>();
+            rollStateHash = Animator.StringToHash(rollStateName);
         }
 
         void Update()
         {
             if (cd > 0f) cd -= Time.deltaTime;
+
+            if ((isDashing || cd > 0f) && inputs.dash)
+                inputs.dash = false;
 
             if (!isDashing && cd <= 0f && TriggerPressed())
             {
@@ -36,13 +46,17 @@ namespace StarterAssets
                 isDashing = true;
                 dashTime = dashDuration;
                 cd = dashCooldown;
+                speedThisDash = dashDistance > 0f && dashDuration > 0f ? dashDistance / dashDuration : dashSpeed;
+
+                if (anim) anim.CrossFadeInFixedTime(rollStateHash, 0f, 0, 0f);
             }
 
             if (isDashing)
             {
-                cc.Move(dashDir * dashSpeed * Time.deltaTime);
+                cc.Move(dashDir * speedThisDash * Time.deltaTime);
                 dashTime -= Time.deltaTime;
-                if (dashTime <= 0f) isDashing = false;
+                if (dashTime <= 0f)
+                    isDashing = false;
             }
         }
 
@@ -56,20 +70,15 @@ namespace StarterAssets
         Vector3 ComputeDir()
         {
             Vector2 m = inputs.move;
-            Vector3 dir;
             if (m.sqrMagnitude > 0.01f && cam != null)
             {
                 Vector3 f = cam.forward; f.y = 0f; f.Normalize();
                 Vector3 r = cam.right; r.y = 0f; r.Normalize();
-                dir = (f * m.y + r * m.x).normalized;
+                return (f * m.y + r * m.x).normalized;
             }
-            else
-            {
-                dir = transform.forward;
-                dir.y = 0f;
-                dir.Normalize();
-            }
-            return dir;
+            Vector3 dir = transform.forward;
+            dir.y = 0f;
+            return dir.normalized;
         }
     }
 }
